83f0719f32985a497c2e2866c5d00c19427ff4de
Joaquin Cid <joaquin@weareadaptive.com>
2020-07-20 16:22:25 +0200

    align display text to center

--- stop-watch/src/App.js
+++ stop-watch/src/App.js
@@ -4,11 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <div id="display" className="display">00:00,00</div>
+      <div id="display" className ="display">00:00,00</div>
       
-      <div id="buttons" class="buttons">
-        <button id="lapResetBtn" class="button buttonLapWhenIsInitial" >Lap</button>
-        <button id="startStopBtn" class="button buttonStart">Start</button>
+      <div id="buttons" className ="buttons">
+        <button id="lapResetBtn" className="button buttonLapWhenIsInitial" >Lap</button>
+        <button id="startStopBtn" className="button buttonStart">Start</button>
       </div>
 
       <div id="laps" />
