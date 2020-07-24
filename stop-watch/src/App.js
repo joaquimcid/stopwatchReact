83f9f0dc5a936fd8eb35d852017d58024e346ec0
Joaquin Cid <joaquin@weareadaptive.com>
2020-07-24 12:47:17 +0200

    WIP

--- stop-watch/src/App.js
+++ stop-watch/src/App.js
@@ -11,66 +11,60 @@ export default function App() {
   const [state, stateChange] = useReducer((currentState, action) =>  {
     switch(action.type) {
       case "START":
-        return [
-          "STARTED",
-          {
+        return {
+            status: "STARTED",
             startedTime: Date.now(),
+            pausedTime: null,
             laps: []
-          }
-        ];
+          };
+
       case "PAUSE":
-        return [
-          "PAUSED",
-          {
-            startedTime: Date.now(),
+        return {
+            status: "PAUSED",      
+            startedTime: currentState.startedTime,
+            pausedTime: Date.now(),
             laps: currentState.laps
-          }
-        ];
+          };
           
       case "CONTINUE":
-        return [
-          "STARTED",
-          {
+        return {
+            status: "STARTED",
             startedTime: Date.now(),
             laps: []
-          }
-        ];
+          };
 
       case "NEWLAP":
         const previousLap = currentState.laps === null || currentState.laps.length === 0 ? currentState.startedTime : currentState.laps[currentState.laps.length-1]; 
         const newLap = Date.now() - previousLap;
         
-        return [
-          "STARTED",
-          {
+        return {
+            status: "STARTED",
             startedTime: currentState.startedTime,
             laps: currentState.laps.concat(newLap)
-          }
-        ];
+          };
+
       case "RESET":
-        return [
-          "INITIAL",
-          {
+        return {
+            status: "INITIAL",
             startedTime: 0,
             laps: []
-          }
-        ];
-      default:
-        return currentState;
+          };
+      default: return currentState;
     }
-  }, ["INITIAL", {
+  }, 
+  {
+    status: "INITIAL",
     startedTime: 0,
     laps: []
-  }]);
+  });
 
   return (
     <div className="app">
-      <Display timeToShow={state[0] === "INITIAL" ? null : Date.now() - state[1].startedTime} />
-      {/* stateChange */}
-            
-      <Buttons status = {state[0]} onButtonClick = {(a) => stateChange({ type: a })}/>
+      <Display timeToShow={state.status === "INITIAL" ? null : Date.now() - state.startedTime} />
+      {/* stateChange */}            
+      <Buttons status = {state.status} onButtonClick = {(a) => stateChange({ type: a })}/>
       {/* <Buttons status = {status} onButtonClick = {(a) =>  Command(a)}/> */}
-      {/* <Laps lapRecords= {state[1].laps}/> */}
+      {/* <Laps lapRecords= {state.laps}/> */}
     </div>
   );
 }
