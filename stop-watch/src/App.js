0f5f63daa51d8dafd22d943ca3d36fad6f4339c0
Joaquin Cid <joaquin@weareadaptive.com>
2020-07-24 16:09:03 +0200

    remove debug callMe method extract dispatch method from component

--- stop-watch/src/App.js
+++ stop-watch/src/App.js
@@ -5,6 +5,61 @@ import Buttons from './components/Buttons';
 
 import './App.css';
 
+const initialState = {
+  status: "INITIAL",
+  startedTime: 0,
+  pausedTime: null,
+  laps: []
+};
+
+function reducer(currentState, action) {
+  console.log(action.type);
+
+  switch(action.type) {
+    case "START":
+      return {
+          status: "STARTED",
+          startedTime: Date.now(),
+          pausedTime: null,
+          laps: []
+        };
+
+    case "PAUSE":
+      return {
+          status: "PAUSED",      
+          startedTime: currentState.startedTime,
+          pausedTime: Date.now(),
+          laps: currentState.laps
+        };
+        
+    case "CONTINUE":
+      return {
+          status: "STARTED",
+          startedTime: currentState.startedTime + Date.now() - currentState.pausedTime,
+          pausedTime: null,
+          laps: currentState.laps
+        };
+
+    case "NEWLAP":
+      const previousLap = currentState.laps === null || currentState.laps.length === 0 
+                        ? currentState.startedTime 
+                        : currentState.laps[currentState.laps.length-1]; 
+      const newLap = Date.now() - previousLap;
+      
+      return {
+          status: "STARTED",
+          startedTime: currentState.startedTime,
+          pausedTime: null,
+          laps: currentState.laps.concat(newLap)
+        };
+
+    case "RESET":
+      return initialState;
+    default: return currentState;
+  }
+  
+}
+
 // TODO :
 // Laps pars no guarden el valor correct. El reducer action dispatch es executa dos cops cada click, revisar.
 export default function App() {
@@ -12,73 +67,13 @@ export default function App() {
   //                    \     /
   //                    PAUSED
   
-  const initialState = {
-    status: "INITIAL",
-    startedTime: 0,
-    pausedTime: null,
-    laps: []
-  };
-
-  function reducer(currentState, action) {
-    console.log(action.type);
-
-    switch(action.type) {
-      case "START":
-        return {
-            status: "STARTED",
-            startedTime: Date.now(),
-            pausedTime: null,
-            laps: []
-          };
-
-      case "PAUSE":
-        return {
-            status: "PAUSED",      
-            startedTime: currentState.startedTime,
-            pausedTime: Date.now(),
-            laps: currentState.laps
-          };
-          
-      case "CONTINUE":
-        return {
-            status: "STARTED",
-            startedTime: currentState.startedTime + Date.now() - currentState.pausedTime,
-            pausedTime: null,
-            laps: currentState.laps
-          };
-
-      case "NEWLAP":
-        const previousLap = currentState.laps === null || currentState.laps.length === 0 
-                          ? currentState.startedTime 
-                          : currentState.laps[currentState.laps.length-1]; 
-        const newLap = Date.now() - previousLap;
-        
-        return {
-            status: "STARTED",
-            startedTime: currentState.startedTime,
-            pausedTime: null,
-            laps: currentState.laps.concat(newLap)
-          };
-
-      case "RESET":
-        return initialState;
-      default: return currentState;
-    }
-    
-  }
-
-  function callMe(a) {
-    console.log('Call me:' + a.type);
-    dispatch(a);
-  }
-  
   // action    => RESET, START, PAUSE, CONTINUE, NEWLAP  
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div className="app">
       <Display timeToShow={state.status === "INITIAL" ? null : Date.now() - state.startedTime} />
-      <Buttons status = {state.status} onButtonClick = {(a) => callMe({ type: a })}/>
+      <Buttons status = {state.status} onButtonClick = {(a) => dispatch({ type: a })}/>
       <Laps lapRecords= {state.laps}/>
     </div>
   );
