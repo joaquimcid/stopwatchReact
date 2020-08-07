a5dfecacc32c1f40bcf51b5531b5cebdca6c004c
Joaquin Cid <joaquin@weareadaptive.com>
2020-08-07 11:51:04 +0200

    no message

--- stop-watch/src/components/Laps.js
+++ stop-watch/src/components/Laps.js
@@ -1,14 +1,5 @@
 import React from 'react';
 
-/*
-lapRecord 
-{
-        index: currentState.laps.length+1,
-        totalTime: Date.now()-currentState.startedTime,
-        isMin: false,
-        isMax: false,
-}
-*/
 export default function Laps({lapRecords, format}) {
   
   console.log('laps component');
